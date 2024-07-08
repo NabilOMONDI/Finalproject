2f2844b20f23e8df93d9b7a276904cab4eb328d8
NabilOMONDI <kaisercap6@gmail.com>
2024-07-08 14:48:32 +0200

    api first try

--- API FMA.py
+++ API FMA.py
@@ -19,6 +19,43 @@ db_config = {
 @app.route('/')
 def home():
     return "Welcome to Nabil's Music API based on FMA dataset"
+
+
 @app.route('/tracks',method=['GET'])
-def track_function():
-    
+def search_tracks_function():
+        page = int(request.args.get('page', 1))
+        per_page = int(request.args.get('per_page', 10))
+        include_details = int(request.args.get('include_details', 0))
+        search = request.args.get('search', '')
+        offset = (page - 1) * per_page
+
+        try:
+              db_conn= pymysql.connect(**db_config)
+              with db_conn.cursor as cursor:
+                    base_query =''' SELECT
+                    t.track_id,
+                    t.track_title,
+                    a.artist_name,
+                    g.genre_handle,
+                    g.genre_id,
+                    a.album_handle
+                    FROM track t
+                    inner join genre g on t.genre_id= g.genre_id
+                    inner join album a on t.album_id = a.album_id'''
+                    if search:
+                        base_query += "WHERE track_title like %s"
+                        search= f"%{search}%"
+
+                    base_query += " ORDER BY track_id LIMIT %s OFFSET %s"
+                    if search:
+                          cursor.execute(base_query,(search, per_page,offset))
+                    else:
+                          cursor.execute(base_query,(per_page,offset))
+                    tracks= cursor.fetchall()
+
+                    if not tracks:
+                          return jsonify({"message":"no track found"}),404
+                db_conn.close()
+                return jsonify(tracks)
+        except pymysql.MySQLError as e:
+            return jsonify({'error':str(e)}),500
